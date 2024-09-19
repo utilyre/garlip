7c95d1ff41310eeb5e721ed88e5ecb3b224dda8e
Amirabbas Fazelinia <utilyre@gmail.com>
2024-09-19 19:13:23 +0330

    Initialize backend and frontend projects

---
+++ backend/cmd/server/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/echo", handleEcho)
+
+	log.Println("Listening on http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", mux))
+}
+
+func handleEcho(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", r.Header.Get("content-type"))
+	w.WriteHeader(http.StatusOK)
+	_, _ = io.Copy(w, r.Body)
+}
