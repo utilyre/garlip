90343be13e24a17cf68afcd40985a12abde04d7e
Amirabbas Fazelinia <utilyre@gmail.com>
2024-10-27 15:30:52 +0330

    make the schema more readable

--- backend/schema.sql
+++ backend/schema.sql
@@ -1,8 +1,8 @@
 CREATE TABLE "accounts" (
     "id" SERIAL PRIMARY KEY,
+
     "created_at" TIMESTAMP NOT NULL,
     "updated_at" TIMESTAMP NOT NULL,
-
     "username" VARCHAR(50) NOT NULL UNIQUE,
     "password" BYTEA NOT NULL,
     "fullname" VARCHAR(100),
@@ -12,65 +12,61 @@ CREATE TABLE "accounts" (
 
 CREATE TABLE "forms" (
     "id" SERIAL PRIMARY KEY,
-    "created_at" TIMESTAMP NOT NULL,
-    "updated_at" TIMESTAMP NOT NULL,
-
     "owner_id" INTEGER REFERENCES "accounts" ON DELETE SET NULL,
 
+    "created_at" TIMESTAMP NOT NULL,
+    "updated_at" TIMESTAMP NOT NULL,
     "title" VARCHAR(100) NOT NULL,
     "description" TEXT
 );
 
 CREATE TABLE "questions" (
     "id" SERIAL PRIMARY KEY,
-    "created_at" TIMESTAMP NOT NULL,
-    "updated_at" TIMESTAMP NOT NULL,
-
     "form_id" INTEGER REFERENCES "forms" ON DELETE SET NULL,
 
+    "created_at" TIMESTAMP NOT NULL,
+    "updated_at" TIMESTAMP NOT NULL,
     "stem" TEXT NOT NULL
 );
 
 CREATE TABLE "topics" (
     "id" SERIAL PRIMARY KEY,
+
     "created_at" TIMESTAMP NOT NULL,
     "updated_at" TIMESTAMP NOT NULL,
-
     "name" VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE "question_topics" (
     "question_id" INTEGER REFERENCES "questions" ON DELETE CASCADE,
     "topic_id" INTEGER REFERENCES "topics" ON DELETE CASCADE,
-    PRIMARY KEY ("question_id", "topic_id"),
 
-    "created_at" TIMESTAMP NOT NULL
+    "created_at" TIMESTAMP NOT NULL,
+
+    PRIMARY KEY ("question_id", "topic_id")
 );
 
 CREATE TABLE "submissions" (
     "id" SERIAL PRIMARY KEY,
-    "created_at" TIMESTAMP NOT NULL,
-
     "respondent_id" INTEGER REFERENCES "accounts" ON DELETE SET NULL,
     "form_id" INTEGER NOT NULL REFERENCES "forms" ON DELETE CASCADE,
 
+    "created_at" TIMESTAMP NOT NULL,
     "note" TEXT
 );
 
 CREATE TABLE "options" (
     "id" SERIAL PRIMARY KEY,
-    "created_at" TIMESTAMP NOT NULL,
-    "updated_at" TIMESTAMP NOT NULL,
-
     "question_id" INTEGER NOT NULL REFERENCES "questions" ON DELETE CASCADE,
 
+    "created_at" TIMESTAMP NOT NULL,
+    "updated_at" TIMESTAMP NOT NULL,
     "description" TEXT NOT NULL,
     "correct" BOOLEAN NOT NULL DEFAULT false
 );
 
 CREATE TABLE "answers" (
     "id" SERIAL PRIMARY KEY,
-
     "submission_id" INTEGER NOT NULL REFERENCES "submissions" ON DELETE CASCADE,
     "question_id" INTEGER NOT NULL REFERENCES "questions" ON DELETE CASCADE,
     "option_id" INTEGER NOT NULL REFERENCES "options" ON DELETE CASCADE,
