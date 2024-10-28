1c7fe42b2d1235a4bed1439dfe401e9f745f309b
Amirabbas Fazelinia <utilyre@gmail.com>
2024-10-29 00:58:05 +0330

    use golang-migrate instead of atlasgo.io

---
+++ backend/migrations/000001_initialize.down.sql
@@ -0,0 +1,8 @@
+DROP TABLE "answers";
+DROP TABLE "options";
+DROP TABLE "submissions";
+DROP TABLE "question_topics";
+DROP TABLE "topics";
+DROP TABLE "questions";
+DROP TABLE "forms";
+DROP TABLE "accounts";
