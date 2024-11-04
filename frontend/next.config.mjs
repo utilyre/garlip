9817539986c5f6a650d67b36a961c6b20ce28e3f
Amirabbas Fazelinia <utilyre@gmail.com>
2024-11-04 22:14:48 +0330

    format with prettier

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	async rewrites() {
-		return [
-			{
-				source: "/api/:path*",
-				destination: "http://backend:80/api/:path*",
-			},
-		]
-	},
-}
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://backend:80/api/:path*",
+      },
+    ];
+  },
+};
 
 export default nextConfig;
