736c2d29e70a0628b594615fb7a0f76fdbe2afda
Amirabbas Fazelinia <utilyre@gmail.com>
2024-11-04 19:02:13 +0330

    make the login page functional

--- frontend/app/auth/login/page.tsx
+++ frontend/app/auth/login/page.tsx
@@ -1,4 +1,26 @@
+"use client";
+
+import { FormEvent, useState } from "react";
+
 export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function onSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const response = await fetch("/api/v1/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    });
+
+    const data = await response.json();
+    console.log(data);
+  }
+
   return (
     <main className="flex h-screen flex-col items-center justify-center p-4">
       <div className="w-full border-2 border-accent-700 bg-accent-50 p-4 sm:w-1/2 md:w-1/3">
@@ -8,7 +30,7 @@ export default function Login() {
 
         <div className="h-6"></div>
 
-        <form className="flex flex-col items-center gap-6">
+        <form onSubmit={onSubmit} className="flex flex-col items-center gap-6">
           <div className="flex w-full flex-col gap-4">
             <div className="flex flex-col">
               <label htmlFor="username" className="text-xs">
@@ -17,6 +39,8 @@ export default function Login() {
               <input
                 id="username"
                 type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
                 placeholder="donaldtrump"
                 className="h-8 border border-foreground p-2 text-sm outline-none"
               />
@@ -29,6 +53,8 @@ export default function Login() {
               <input
                 id="password"
                 type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
                 placeholder="n#tgAL7m"
                 className="h-8 border border-foreground p-2 text-sm outline-none"
               />
