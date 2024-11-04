7c69b80bc359b5cb619bb778580923d14317946a
Amirabbas Fazelinia <utilyre@gmail.com>
2024-11-04 22:47:48 +0330

    style login page

--- frontend/app/auth/login/page.tsx
+++ frontend/app/auth/login/page.tsx
@@ -23,47 +23,41 @@ export default function Login() {
 
   return (
     <main className="flex h-screen flex-col items-center justify-center p-4">
-      <div className="w-full border-2 border-accent-700 bg-accent-50 p-4 sm:w-1/2 md:w-1/3">
-        <h1 className="text-center text-2xl font-light text-foreground">
-          Who are you?
+      <div className="flex w-full flex-col gap-10 sm:w-1/2 md:w-1/3">
+        <h1 className="text-center text-2xl font-bold sm:text-4xl">
+          Login to Garlip
         </h1>
 
-        <div className="h-6"></div>
-
-        <form onSubmit={onSubmit} className="flex flex-col items-center gap-6">
-          <div className="flex w-full flex-col gap-4">
-            <div className="flex flex-col">
-              <label htmlFor="username" className="text-xs">
-                Username
-              </label>
+        <form onSubmit={onSubmit} className="flex flex-col items-center gap-4">
+          <div className="flex w-full flex-col gap-2">
+            <div className="flex flex-col gap-1">
+              <label htmlFor="username">Username</label>
               <input
                 id="username"
                 type="text"
+                required
                 value={username}
                 onChange={(e) => setUsername(e.target.value)}
-                placeholder="donaldtrump"
-                className="h-8 border border-foreground p-2 text-sm outline-none"
+                className="h-12 rounded-md border border-gray-500 bg-background p-2 text-sm outline-none transition hover:border-gray-800 focus:border-gray-400 focus:ring focus:ring-gray-300 dark:hover:border-gray-400 dark:focus:ring-gray-600"
               />
             </div>
 
-            <div className="flex flex-col">
-              <label htmlFor="password" className="text-xs">
-                Password
-              </label>
+            <div className="flex flex-col gap-1">
+              <label htmlFor="password">Password</label>
               <input
                 id="password"
                 type="password"
+                required
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
-                placeholder="n#tgAL7m"
-                className="h-8 border border-foreground p-2 text-sm outline-none"
+                className="h-12 rounded-md border border-gray-500 bg-background p-2 text-sm outline-none transition hover:border-gray-800 focus:border-gray-400 focus:ring focus:ring-gray-300 dark:hover:border-gray-400 dark:focus:ring-gray-600"
               />
             </div>
           </div>
 
           <button
             type="submit"
-            className="h-10 w-40 bg-accent-800 text-sm font-bold text-background outline-none focus:ring-2 focus:ring-accent-800 focus:ring-offset-2"
+            className="h-12 w-full rounded-md bg-foreground text-background outline-none transition-colors hover:bg-gray-800 focus:outline-blue-400 dark:hover:bg-gray-300"
           >
             Login
           </button>
