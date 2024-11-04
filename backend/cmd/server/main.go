02f4a30f262ddb457741ba49cb60b6157a346ca9
Amirabbas Fazelinia <utilyre@gmail.com>
2024-11-04 21:57:57 +0330

    use cors instead of proxy

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -13,6 +13,7 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 	"github.com/jackc/pgx/v5"
 	"github.com/utilyre/xmate/v2"
 )
@@ -41,6 +42,14 @@ func main() {
 
 	mux.Use(middleware.Logger)
 	mux.Use(middleware.Recoverer)
+	mux.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
 	mux.Get("/helloworld", eh.HandleFunc(handleHelloWorld))
 	mux.Mount("/api/v1", apiV1)
 
--- backend/go.mod
+++ backend/go.mod
@@ -4,6 +4,7 @@ go 1.23.0
 
 require (
 	github.com/go-chi/chi/v5 v5.1.0
+	github.com/go-chi/cors v1.2.1
 	github.com/golang-jwt/jwt/v5 v5.2.1
 	github.com/jackc/pgx/v5 v5.7.1
 	golang.org/x/crypto v0.27.0
