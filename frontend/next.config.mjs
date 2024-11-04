9bfb22bbf271f156478886e30ef91bd8187713ca
Amirabbas Fazelinia <utilyre@gmail.com>
2024-11-04 22:00:42 +0330

    fix proxy to backend

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	async rewrites() {
+		return [
+			{
+				source: "/api/:path*",
+				destination: "http://backend:80/api/:path*",
+			},
+		]
+	},
+}
 
 export default nextConfig;
