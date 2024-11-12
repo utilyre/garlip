87cc90b24fab800b0eda5330784e78decd8b108b
Amirabbas Fazelinia <utilyre@gmail.com>
2024-11-12 21:39:51 +0330

    fix(login): use shadcn/ui components

--- frontend/app/auth/login/page.tsx
+++ frontend/app/auth/login/page.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { FormEvent, useState } from "react";
 
 export default function Login() {
@@ -40,44 +43,39 @@ export default function Login() {
 
   return (
     <main className="flex h-screen flex-col items-center justify-center p-4">
-      <div className="flex w-full flex-col gap-10 sm:w-1/2 md:w-1/3">
+      <div className="flex w-full flex-col gap-10 sm:w-1/2 md:w-1/4">
         <h1 className="text-center text-2xl font-bold sm:text-4xl">
           Login to Garlip
         </h1>
 
-        <form onSubmit={onSubmit} className="flex flex-col items-center gap-4">
+        <form onSubmit={onSubmit} className="space-y-4">
           <div className="flex w-full flex-col gap-2">
-            <div className="flex flex-col gap-1">
-              <label htmlFor="username">Username</label>
-              <input
+            <div className="space-y-1">
+              <Label htmlFor="username">Username</Label>
+              <Input
                 id="username"
                 type="text"
                 required
                 value={username}
                 onChange={(e) => setUsername(e.target.value)}
-                className="h-12 rounded-md border border-gray-500 bg-background p-2 text-sm outline-none transition hover:border-gray-800 focus:border-gray-400 focus:ring focus:ring-gray-300 dark:hover:border-gray-400 dark:focus:ring-gray-600"
               />
             </div>
 
-            <div className="flex flex-col gap-1">
-              <label htmlFor="password">Password</label>
-              <input
+            <div className="space-y-1">
+              <Label htmlFor="password">Password</Label>
+              <Input
                 id="password"
                 type="password"
                 required
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
-                className="h-12 rounded-md border border-gray-500 bg-background p-2 text-sm outline-none transition hover:border-gray-800 focus:border-gray-400 focus:ring focus:ring-gray-300 dark:hover:border-gray-400 dark:focus:ring-gray-600"
               />
             </div>
           </div>
 
-          <button
-            type="submit"
-            className="h-12 w-full rounded-md bg-foreground text-background outline-none transition-colors hover:bg-gray-800 focus:outline-blue-400 dark:hover:bg-gray-300"
-          >
+          <Button type="submit" className="w-full">
             Login
-          </button>
+          </Button>
         </form>
       </div>
     </main>
